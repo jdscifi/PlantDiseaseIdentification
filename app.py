acb5bc3a1ab7f15cef47175b7ea01e435c64cb8a
jdscifi <jayduttjoshi@gmail.com>
2022-04-06 20:33:01 -0400

    Final changes to prediction logic

--- app.py
+++ app.py
@@ -1,33 +1,79 @@
+import json
 import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 import numpy as np
-from tensorflow import keras
-from skimage import io
-from tensorflow.keras.preprocessing import image
-
+import tensorflow_hub as hub
 from flask import Flask, render_template, request, url_for, flash, redirect, jsonify
 from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
 
+tf.get_logger().setLevel('ERROR')
 app = Flask(__name__)
 
-model =tf.keras.models.load_model('PlantDNet.h5',compile=False)
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
-def model_predict(img_path, model):
-    img = image.load_img(img_path, grayscale=False, target_size=(144, 144))
-    show_img = image.load_img(img_path, grayscale=False, target_size=(144, 144))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = np.array(x, 'float32')
-    x /= 255
-    preds = model.predict(x)
-    return preds
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route ('/')
+json_path = "models/final_model.json"
+h5_path = "models/final_model.h5"
+
+IMAGE_INPUT_SIZE = (144, 144)
+
+with open("infection_info.json", "r") as ii:
+    infection_info = json.load(ii)
+
+
+def load_model():
+    try:
+        with open(json_path, 'r') as json_file:
+            loaded_model_json = json_file.read()
+        loaded_model = tf.keras.models.model_from_json(loaded_model_json, custom_objects={'KerasLayer': hub.KerasLayer})
+        loaded_model.load_weights(h5_path)
+        loaded_model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.005, momentum=0.9),
+                             loss='categorical_crossentropy', metrics=['accuracy'])
+        return loaded_model
+    except Exception:
+        return False
+
+
+model = load_model()
+
+
+def file_upload(request):
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file uploaded')
+            return redirect(request.url)
+        file = request.files['file']
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+
+def model_predict(file_name):
+    global model
+    image_input = tf.keras.utils.img_to_array(
+        tf.keras.utils.load_img(file_name, grayscale=False, target_size=IMAGE_INPUT_SIZE))
+    image_input = np.array(np.expand_dims(image_input, axis=0), 'float32') / 255
+    return model.predict(image_input, verbose=0)
+
+
+@app.route('/')
 def home():
-	return render_template(".html", )
+    return render_template("form.html")
 
- 
- if __name__ == "__main__":
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    score = np.argmax(model_predict(file_upload(request)), axis=1)
+    return render_template("output.html", infection=infection_info[score[0]]["infection"],
+                           solution=infection_info[score[0]]["solution"])
+
+
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(port = port)
+    app.run(port=port)
